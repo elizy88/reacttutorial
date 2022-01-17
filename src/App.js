d3a5ba228b6cdf4384409ea4989ebbe7b2a78972
Elsabeth Kidane <elsabethkidane@Elsabeths-MacBook-Pro.local>
2022-01-17 15:56:33 -0500

    created to do list using map function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+let todoList=[
+  {id:1, title:'The bird'},
+  {id:2, title:'The moon'},
+  {id :3, title:'The star'}
+  ]
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+return(
+    <div>
+    <h1>Todo List</h1>
+    {todoList.map((item)=>(
+      <li key={item.id}>{item.title}</li>
+ 
+    ))}
+ 
+  </div>
   );
 }
 
 export default App;
+
